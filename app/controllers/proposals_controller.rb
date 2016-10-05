d44a586f5aed80c23e80b747806b95c70c67f911
peterberwind <peterberwind@gmail.com>
2016-10-04 20:07:29 -0400

    adding for to create new proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -3,5 +3,20 @@ class ProposalsController < ApplicationController
   def index
     @proposals = Proposal.all
   end
-  
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.create(proposal_params)
+    redirect_to proposals_path
+  end
+
+  private
+
+  def proposal_params
+    params.require(:proposal).permit(:title, :status)
+  end
+
 end

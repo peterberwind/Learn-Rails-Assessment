ad5812edadf5915725c219add43f0466e8b2481a
peterberwind <peterberwind@gmail.com>
2016-10-12 01:37:22 -0400

    adding demo chart for soon to be dashabord

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery
 //= require bootstrap-sprockets
+//= require chart
 //= require adminLTE
 //= require_tree .

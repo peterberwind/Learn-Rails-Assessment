8a7dc33b339819f354badc00eb3b4bcad35273ee
peterberwind <peterberwind@gmail.com>
2016-10-12 16:41:06 -0400

    adding dashobard and chart

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/main'
   devise_for :users
   resources :clients
   resources :proposals
   resources :projects
-  root "projects#index"
+  root "dashboard#main"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
